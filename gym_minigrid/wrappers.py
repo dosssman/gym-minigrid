32a0cab6ddb2e1605d63d490764e0374f8ad902e
dosssman <dosssman@hotmail.fr>
2020-05-25 10:59:16 +0900

    Added FlatObsWrapperNoMission to support flattening of envs stripped of mission and language

--- gym_minigrid/wrappers.py
+++ gym_minigrid/wrappers.py
@@ -247,6 +247,31 @@ class FullyObsWrapper(gym.core.ObservationWrapper):
             'image': full_grid
         }
 
+class FlatObsWrapperNoMission(gym.core.ObservationWrapper):
+    """
+    Encode mission strings using a one-hot scheme,
+    and combine these with observed images into one flat array
+    """
+
+    def __init__(self, env, maxStrLen=96):
+        super().__init__(env)
+
+        imgSpace = env.observation_space
+
+        self.observation_space = spaces.Box(
+            low=0,
+            high=255,
+            shape=imgSize,
+            dtype='uint8'
+        )
+
+        self.cachedStr = None
+        self.cachedArray = None
+
+    def observation(self, obs):
+
+        return obs.flatten()
+
 class FlatObsWrapper(gym.core.ObservationWrapper):
     """
     Encode mission strings using a one-hot scheme,
